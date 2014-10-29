36809b105c2b591cdaea8bf7252207b3ce5769bc
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-10-28 21:27:41 -0400

    Project 4 Code

---
+++ Project4_KoltonGreen.R
@@ -0,0 +1,14 @@
+#======================================================================================
+#Name: Kolton D. Green
+#Date: 10/28/2014
+#Course: IS 360 02
+#Project 4
+#Summary: Pulls treasury rates since the beginning of the month. 
+#======================================================================================
+# treasury <- html("http://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yield")
+# rates <- treasury %>%
+#  html_nodes(".text_view_data , th") %>%
+#  html_text() %>%
+#  as.data.frame()
+
+
